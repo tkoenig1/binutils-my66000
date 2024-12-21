763086a4d26c5b1587b4a4f1e15dd90ac17c2bd4
Thomas Koenig <tkoenig@gcc.gnu.org>
2024-12-21 08:54:27 +0100

    Correct syntax for LDM/STM and add base+offset.

--- opcodes/my66000-opc.c
+++ opcodes/my66000-opc.c
@@ -2111,7 +2111,9 @@ static const my66000_fmt_spec_t ms_60_fmt_list[] =
 
 static const my66000_fmt_spec_t ldm_fmt_list [] =
 {
-  { "A,B,C", 0, XOP1_FLAGS_MASK},
+  { "A,B,[C]",   0, XOP1_FLAGS_MASK},
+  { "A,B,[C,L]", XOP1_BITS(1,0), XOP1_FLAGS_MASK},
+  { "A,B,[C,L]", XOP1_BITS(1,1), XOP1_FLAGS_MASK},
   { NULL, 0, 0},
 };
 
